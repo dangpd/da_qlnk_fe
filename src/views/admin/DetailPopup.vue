653c2f2748da2e323a127c34e677c9b7b65a20fc
dangpd <phamducdang0403@gmail.com>
2024-06-29 13:20:41 +0700

    update

--- src/views/admin/DetailPopup.vue
+++ src/views/admin/DetailPopup.vue
@@ -39,11 +39,13 @@ export default {
   z-index: 100;
   left: 0;
   top: 0;
-  width: 100%;
-  height: 100%;
-  overflow: auto;
+  width: 100vw;
+  height: 100vh;
   background-color: rgb(0, 0, 0);
   background-color: rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .popup-content {
